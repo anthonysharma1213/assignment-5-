033399b34a17fdb8399c14df14c7e7549ef6be86
anthonysharma1213 <120972000+anthonysharma1213@users.noreply.github.com>
2023-06-24 17:35:19 -0400

    Update lab_assignment_6 (1).c

--- lab_assignment_6 (1).c
+++ lab_assignment_6 (1).c
@@ -5,16 +5,16 @@ int search(int numbers[], int low, int high, int value)
 	if (low>high){
 	return -1; 
 	}
-	int mid= low+(low-high)/2;
+	int mid= low+(high-low)/2;
 	
 	if(numbers[mid]==value){
 	return mid;
 	}
 	else if (numbers[mid]>value){
-	return search(numbers,low,mid,-1,value);
+	return search(numbers,low,mid,high,value);
 	}
 	else{
-	return search(numbers,low,mid-1,value);
+	return search(numbers,low,mid-1,high,value);
 	}
 	
 }
